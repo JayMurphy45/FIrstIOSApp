ca2c40cfa7d6eaa30446b5e90ba6ca55c0a9cf32
JayMurphy45 <B00143682@mytudublin.ie>
2024-04-18 16:56:09 +0100

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Jamie Murphy on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
